5cbf58c115e41c56d6d262ffe0d0441ff92bec81
Ezequiel Parziale <eparziale@gmail.com>
2022-09-02 19:22:55 -0300

    :rocket: update: use settings vars

--- app/main.py
+++ app/main.py
@@ -1,7 +1,11 @@
 from fastapi import FastAPI
 
 from app.api.api_v1.api import api_router
+from app.core.config import settings
 
-app = FastAPI()
+app = FastAPI(
+    title=settings.PROJECT_NAME,
+    openapi_url=f"{settings.API_V1_STR}/openapi.json"
+)
 
-app.include_router(api_router, prefix="/v1")
+app.include_router(api_router, prefix=settings.API_V1_STR)
