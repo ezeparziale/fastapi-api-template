4c15824d52bc015a5cd4d831b04d7d02d8f44f85
Ezequiel Parziale <eparziale@gmail.com>
2022-09-02 22:22:13 -0300

    :bulb: update: add info

--- app/api/auth.py
+++ app/api/auth.py
@@ -7,13 +7,17 @@ auth_router = APIRouter()
 
 @auth_router.get("/login")
 async def login(request: Request):
-    """ """
+    """
+    ### Login
+    """
     redirect_uri = request.url_for("authorize")
     return await oauth.google.authorize_redirect(request, redirect_uri)
 
 
 @auth_router.get("/authorize")
 async def authorize(request: Request):
-    """ """
+    """
+    ### Authorize
+    """
     token = await oauth.google.authorize_access_token(request)
     return token
