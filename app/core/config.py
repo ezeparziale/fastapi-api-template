4af4ff776ab1e6bdba705c524f2a05e668ec8787
Ezequiel Parziale <eparziale@gmail.com>
2022-09-02 21:34:15 -0300

    :wrench: update: add new vars

--- app/core/config.py
+++ app/core/config.py
@@ -1,11 +1,16 @@
 from pydantic import BaseSettings
 
 class Settings(BaseSettings):
+    # FastAPI
     API_V1_STR: str = "/api/v1"
     PROJECT_NAME: str = "FastAPI Google Auth Login"
+    SECRET_KY: str
 
+    # Google
     GOOGLE_CLIENT_ID: str
     GOOGLE_CLIENT_SECRET: str
+    GOOGLE_SCOPES: str = "openid email profile"
+    GOOGLE_CONF_URL: str = "https://accounts.google.com/.well-known/openid-configuration"
 
     class Config:
         env_file = ".env"
