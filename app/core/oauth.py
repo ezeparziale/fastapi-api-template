2ea4d829507e9621eb01828fe1523c84e2bfbf7d
Ezequiel Parziale <eparziale@gmail.com>
2022-09-02 23:49:51 -0300

    :rocket: update: add token functions

--- app/core/oauth.py
+++ app/core/oauth.py
@@ -1,6 +1,21 @@
+from datetime import datetime, timedelta
+
 from authlib.integrations.starlette_client import OAuth
 
 from app.core.config import settings
+from jose import jwt
+from jose.exceptions import JWTError
+from app.core.config import settings
+from app.models import User
+from fastapi import status, Depends, HTTPException
+from sqlalchemy.orm import Session
+from app.db.database import get_db
+from app.schemas import TokenData
+from fastapi.security import OAuth2PasswordBearer
+
+SECRET_KEY = settings.SECRET_KEY
+ALGORITHM = settings.ALGORITHM
+ACCESS_TOKEN_EXPIRE_MINUTES = settings.ACCESS_TOKEN_EXPIRE_MINUTES
 
 oauth = OAuth()
 
@@ -11,3 +26,39 @@ oauth.register(
     server_metadata_url=settings.GOOGLE_CONF_URL,
     client_kwargs={"scope": settings.GOOGLE_SCOPES},
 )
+
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="/login")
+
+def create_access_token(data: dict):
+    to_encode = data.copy()
+    expire = datetime.utcnow() + timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
+    to_encode.update({"exp": expire})
+    encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
+    return encoded_jwt
+
+def verify_access_token(token: str, credentials_exception):
+    try:
+        payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
+        id: str = payload.get("user_id")
+
+        if id is None:
+            raise credentials_exception
+        token_data = TokenData(id=id)
+    except JWTError:
+        raise credentials_exception
+
+    return token_data
+
+def get_current_user(
+    token: str = Depends(oauth2_scheme), db: Session = Depends(get_db)
+):
+    credentials_exception = HTTPException(
+        status_code=status.HTTP_401_UNAUTHORIZED,
+        detail="Could not validate credentials",
+        headers={"WWW-Authenticate": "Bearer"},
+    )
+
+    token = verify_access_token(token, credentials_exception)
+
+    user = db.query(User).filter(User.id == token.id).first()
+    return user
