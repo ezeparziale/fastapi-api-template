72e1c03b2bce193f335e5d8b01dd8e6c9dcba591
Ezequiel Parziale <eparziale@gmail.com>
2022-09-02 22:19:39 -0300

    :wrench: update: add db vars

--- app/core/config.py
+++ app/core/config.py
@@ -1,11 +1,11 @@
-from pydantic import BaseSettings
-
+from pydantic import BaseSettings, PostgresDsn, validator
+from typing import Optional, Dict, Any
 
 class Settings(BaseSettings):
     # FastAPI
     API_V1_STR: str = "/api/v1"
     PROJECT_NAME: str = "FastAPI Google Auth Login"
-    SECRET_KY: str
+    SECRET_KEY: str
 
     # Google
     GOOGLE_CLIENT_ID: str
@@ -14,6 +14,28 @@ class Settings(BaseSettings):
     GOOGLE_CONF_URL: str = (
         "https://accounts.google.com/.well-known/openid-configuration"
     )
+
+    # Database
+    POSTGRES_HOSTNAME: str
+    POSTGRES_PORT: str
+    POSTGRES_USER: str
+    POSTGRES_PASSWORD: str
+    POSTGRES_DB: str
+
+    SQLALCHEMY_DATABASE_URI: Optional[PostgresDsn] = None
+
+    @validator("SQLALCHEMY_DATABASE_URI", pre=True)
+    def assemble_db_connection(cls, v: Optional[str], values: Dict[str, Any]) -> Any:
+        if isinstance(v, str):
+            return v
+        return PostgresDsn.build(
+            scheme="postgresql",
+            user=values.get("POSTGRES_USER"),
+            password=values.get("POSTGRES_PASSWORD"),
+            host=values.get("POSTGRES_HOSTNAME"),
+            port=values.get("POSTGRES_PORT"),
+            path=f"/{values.get('POSTGRES_DB') or ''}",
+        )
 
     class Config:
         env_file = ".env"
