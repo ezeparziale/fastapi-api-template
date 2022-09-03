7f715af75294a33bf344684a26a30d7efd6b9f67
Ezequiel Parziale <eparziale@gmail.com>
2022-09-02 23:50:11 -0300

    :rocket: update: auth token access

--- app/api/auth.py
+++ app/api/auth.py
@@ -1,6 +1,8 @@
-from fastapi import APIRouter, Request
-
-from app.core.oauth import oauth
+from fastapi import APIRouter, Request, Depends, status, HTTPException
+from sqlalchemy.orm import Session
+from app.db.database import get_db
+from app.models import User
+from app.core.oauth import oauth, create_access_token
 
 auth_router = APIRouter()
 
@@ -15,9 +17,17 @@ async def login(request: Request):
 
 
 @auth_router.get("/authorize")
-async def authorize(request: Request):
+async def authorize(request: Request, db: Session = Depends(get_db)):
     """
     ### Authorize
     """
     token = await oauth.google.authorize_access_token(request)
-    return token
+    user = db.query(User).filter(User.email == token["userinfo"]["email"]).first()
+    
+    if not user:
+        raise HTTPException(
+            status_code=status.HTTP_403_FORBIDDEN, detail="Invalid Credentials"
+        )
+
+    access_token = create_access_token(data={"user_id": user.id})
+    return {"access_token": access_token, "token_type": "bearer"}
