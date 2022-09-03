0b652148f88602302545c963c40b8b6f0d664602
Ezequiel Parziale <eparziale@gmail.com>
2022-09-02 21:34:35 -0300

    :rocket: update: add auth endpoint

---
+++ app/api/auth.py
@@ -0,0 +1,22 @@
+from fastapi import APIRouter
+from fastapi import Request
+from starlette.requests import Request
+from app.core.oauth import oauth
+
+auth_router = APIRouter()
+ 
+
+@auth_router.get("/login")
+async def login(request: Request):
+    """
+    """
+    redirect_uri = request.url_for('authorize')
+    return await oauth.google.authorize_redirect(request, redirect_uri)
+
+
+@auth_router.get("/authorize")
+async def authorize(request: Request):
+    """
+    """
+    token = await oauth.google.authorize_access_token(request)
+    return token
--- app/main.py
+++ app/main.py
@@ -1,11 +1,19 @@
 from fastapi import FastAPI
 
 from app.api.api_v1.api import api_router
+from app.api.auth import auth_router
 from app.core.config import settings
+from starlette.middleware.sessions import SessionMiddleware
 
+# FastAPI
 app = FastAPI(
     title=settings.PROJECT_NAME,
     openapi_url=f"{settings.API_V1_STR}/openapi.json"
 )
 
+app.add_middleware(SessionMiddleware, secret_key=settings.SECRET_KY)
+
+
+# Routes
 app.include_router(api_router, prefix=settings.API_V1_STR)
+app.include_router(auth_router, tags=["authorization"])
