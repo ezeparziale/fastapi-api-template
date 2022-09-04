2f1cb50523f8e564ae1ab94eb98b63a226159183
Ezequiel Parziale <eparziale@gmail.com>
2022-09-04 18:12:34 -0300

    :rocket: update: add basic login

--- app/api/auth.py
+++ app/api/auth.py
@@ -3,14 +3,17 @@ from sqlalchemy.orm import Session
 from app.db.database import get_db
 from app.models import User
 from app.core.oauth import oauth, create_access_token
+from app.schemas import Token
+from fastapi.security.oauth2 import OAuth2PasswordRequestForm
+from app.utils import utils
 
 auth_router = APIRouter()
 
 
-@auth_router.get("/login")
-async def login(request: Request):
+@auth_router.get("/login_google")
+async def login_google(request: Request):
     """
-    ### Login
+    ### Login Google
     """
     redirect_uri = request.url_for("authorize")
     return await oauth.google.authorize_redirect(request, redirect_uri)
@@ -25,6 +28,31 @@ async def authorize(request: Request, db: Session = Depends(get_db)):
     user = db.query(User).filter(User.email == token["userinfo"]["email"]).first()
     
     if not user:
+        raise HTTPException(
+            status_code=status.HTTP_403_FORBIDDEN, detail="Invalid Credentials"
+        )
+
+    access_token = create_access_token(data={"user_id": user.id})
+    return {"access_token": access_token, "token_type": "bearer"}
+
+
+@auth_router.post("/login", response_model=Token)
+def login(
+    user_credentials: OAuth2PasswordRequestForm = Depends(),
+    db: Session = Depends(get_db),
+):
+    user = (
+        db.query(User)
+        .filter(User.email == user_credentials.username)
+        .first()
+    )
+
+    if not user:
+        raise HTTPException(
+            status_code=status.HTTP_403_FORBIDDEN, detail="Invalid Credentials"
+        )
+
+    if not utils.verify(user_credentials.password, user.password):
         raise HTTPException(
             status_code=status.HTTP_403_FORBIDDEN, detail="Invalid Credentials"
         )
