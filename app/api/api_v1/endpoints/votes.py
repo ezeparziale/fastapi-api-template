ebb40f97945c0199e280fac303e9f85c02937f3d
Ezequiel Parziale <eparziale@gmail.com>
2022-09-04 21:16:21 -0300

    :rocket: update: add vote endpoint

---
+++ app/api/api_v1/endpoints/votes.py
@@ -0,0 +1,48 @@
+from fastapi import APIRouter, Depends, HTTPException, status
+from sqlalchemy.orm import Session
+
+from app.core.oauth import get_current_user
+from app.db.database import get_db
+from app.models import Post, Vote
+from app.schemas import Vote as VoteSchema
+
+router = APIRouter()
+
+
+@router.post("/", status_code=status.HTTP_201_CREATED)
+def vote(
+    vote: VoteSchema,
+    db: Session = Depends(get_db),
+    current_user: int = Depends(get_current_user),
+):
+
+    post = db.query(Post).filter(Post.id == vote.post_id).first()
+    if not post:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail=f"Post with id: {vote.post_id} does not exist",
+        )
+
+    vote_query = db.query(Vote).filter(
+        Vote.post_id == vote.post_id, Vote.user_id == current_user.id
+    )
+
+    found_vote = vote_query.first()
+    if vote.dir == 1:
+        if found_vote:
+            raise HTTPException(
+                status_code=status.HTTP_409_CONFLICT,
+                detail=f"user {current_user.id} has already voted on post {vote.post_id}",
+            )
+        new_vote = Vote(post_id=vote.post_id, user_id=current_user.id)
+        db.add(new_vote)
+        db.commit()
+        return {"message": "successfully added vote"}
+    else:
+        if not found_vote:
+            raise HTTPException(
+                status_code=status.HTTP_404_NOT_FOUND, detail="Vote does not exist"
+            )
+        vote_query.delete(synchronize_session=False)
+        db.commit()
+        return {"message": "successfully deleted vote"}
