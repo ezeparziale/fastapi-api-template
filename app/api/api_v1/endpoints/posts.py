c830cdbaad96de83d1b248f03e5f190dc91c9b58
Ezequiel Parziale <eparziale@gmail.com>
2022-09-04 21:16:09 -0300

    :rocket: update: add post endpoint

---
+++ app/api/api_v1/endpoints/posts.py
@@ -0,0 +1,118 @@
+from fastapi import APIRouter, Depends, HTTPException, Response, status
+from sqlalchemy import func
+from sqlalchemy.orm import Session
+
+from app.core.oauth import get_current_user
+from app.db.database import get_db
+from app.models import Post, Vote
+from app.schemas import Post as PostSchema
+from app.schemas import PostCreate, PostOUT
+
+router = APIRouter()
+
+
+@router.get("/", response_model=list[PostOUT])
+def get_posts(
+    db: Session = Depends(get_db),
+    current_user: int = Depends(get_current_user),
+    limit: int = 10,
+    skip: int = 0,
+    search: str | None = "",
+):
+    posts = (
+        db.query(Post, func.count(Vote.post_id).label("votes"))
+        .join(Vote, Vote.post_id == Post.id, isouter=True)
+        .group_by(Post.id)
+        .filter(Post.title.contains(search))
+        .limit(limit)
+        .offset(skip)
+        .all()
+    )
+    return posts
+
+
+@router.post("/", status_code=status.HTTP_201_CREATED, response_model=PostSchema)
+def create_posts(
+    post: PostCreate,
+    db: Session = Depends(get_db),
+    current_user: int = Depends(get_current_user),
+):
+    new_post = Post(owner_id=current_user.id, **post.dict())
+    db.add(new_post)
+    db.commit()
+    db.refresh(new_post)
+    return new_post
+
+
+@router.get("/{id}", response_model=PostOUT)
+def get_post(
+    id: int,
+    db: Session = Depends(get_db),
+    current_user: int = Depends(get_current_user),
+):
+    post = (
+        db.query(Post, func.count(Vote.post_id).label("votes"))
+        .join(Vote, Vote.post_id == Post.id, isouter=True)
+        .group_by(Post.id)
+        .filter(Post.id == id)
+        .first()
+    )
+
+    if not post:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND, detail=f"Post with id {id} not found"
+        )
+    return post
+
+
+@router.delete("/{id}", status_code=status.HTTP_204_NO_CONTENT)
+def delete_post(
+    id: int,
+    db: Session = Depends(get_db),
+    current_user: int = Depends(get_current_user),
+):
+    post_query = db.query(Post).filter(Post.id == id)
+
+    post = post_query.first()
+
+    if post is None:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail=f"Post with id {id} does not exist",
+        )
+
+    if post.owner_id != current_user.id:
+        raise HTTPException(
+            status_code=status.HTTP_403_FORBIDDEN,
+            detail="Not authorized to perform requested action",
+        )
+
+    post_query.delete(synchronize_session=False)
+    db.commit()
+    return Response(status_code=status.HTTP_204_NO_CONTENT)
+
+
+@router.put("/{id}", response_model=PostSchema)
+def update_post(
+    id: int,
+    post: PostCreate,
+    db: Session = Depends(get_db),
+    current_user: int = Depends(get_current_user),
+):
+    post_query = db.query(Post).filter(Post.id == id)
+    post_to_update = post_query.first()
+    if post_to_update is None:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail=f"Post with id {id} does not exist",
+        )
+
+    if post_to_update.owner_id != current_user.id:
+        raise HTTPException(
+            status_code=status.HTTP_403_FORBIDDEN,
+            detail="Not authorized to perform requested action",
+        )
+
+    post_query.update(post.dict(), synchronize_session=False)
+    db.commit()
+    return post_query.first()
