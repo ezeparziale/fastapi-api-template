c984d9294819c43e4bba2b741e17c5d18f9f9d71
Ezequiel Parziale <eparziale@gmail.com>
2022-09-04 21:28:13 -0300

    :fire: update: delete endpoint

--- app/api/api_v1/api.py
+++ app/api/api_v1/api.py
@@ -5,6 +5,5 @@ from app.api.api_v1.endpoints import items, posts, users, votes
 api_router = APIRouter()
 
 api_router.include_router(users.router, prefix="/users", tags=["users"])
-api_router.include_router(items.router, prefix="/items", tags=["items"])
 api_router.include_router(posts.router, prefix="/posts", tags=["posts"])
 api_router.include_router(votes.router, prefix="/votes", tags=["votes"])
--- app/api/api_v1/endpoints/items.py
+++
@@ -1,9 +0,0 @@
-from fastapi import APIRouter
-
-router = APIRouter()
-
-
-@router.get("/")
-def get_items():
-    """ """
-    return {"id": 1}
