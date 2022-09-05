6d703370f609dc95155b07a0f13a6fc4917fdb1b
Ezequiel Parziale <eparziale@gmail.com>
2022-09-04 21:15:24 -0300

    :rocket: update: base import

--- alembic/env.py
+++ alembic/env.py
@@ -6,7 +6,7 @@ from sqlalchemy import pool
 from alembic import context
 
 from app.core.config import settings
-from app.db.database import Base
+from app.models import Base
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
 config = context.config
