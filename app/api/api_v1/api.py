5ec71734568ecdf6e13ccabab8cdb57c33d76129
Ezequiel Parziale <eparziale@gmail.com>
2022-12-30 18:10:22 -0300

    :rocket: update: order routers

--- app/api/api_v1/api.py
+++ app/api/api_v1/api.py
@@ -5,6 +5,6 @@ from app.api.api_v1.endpoints import auth, posts, users, votes
 api_router = APIRouter()
 
 api_router.include_router(auth.router, tags=["authorization"])
-api_router.include_router(users.router, prefix="/users", tags=["users"])
 api_router.include_router(posts.router, prefix="/posts", tags=["posts"])
+api_router.include_router(users.router, prefix="/users", tags=["users"])
 api_router.include_router(votes.router, prefix="/votes", tags=["votes"])
