76f37e002201836b8da990b2b9577f6e4ce8fde0
Ezequiel Parziale <eparziale@gmail.com>
2023-02-06 20:23:39 -0300

    :rotating_light: update: fix mypy alerts

--- alembic/env.py
+++ alembic/env.py
@@ -59,7 +59,7 @@ def run_migrations_online() -> None:
 
     """
     connectable = engine_from_config(
-        config.get_section(config.config_ini_section),
+        config.get_section(config.config_ini_section),  # type: ignore[arg-type]
         prefix="sqlalchemy.",
         poolclass=pool.NullPool,
     )
