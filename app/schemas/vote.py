dca7e68d761d8506209999aaca061539a0418f05
Ezequiel Parziale <eparziale@gmail.com>
2023-05-08 20:50:54 -0300

    :rotating_light: update: fix lint

--- app/schemas/vote.py
+++ app/schemas/vote.py
@@ -3,7 +3,7 @@ from pydantic import BaseModel, Field, conint
 
 class Vote(BaseModel):
     post_id: int = Field(title="ID of the post", example="1")
-    dir: conint(ge=0, le=1) = Field(
+    dir: conint(ge=0, le=1) = Field(  # type: ignore
         title="Vote direction",
         description="Indicates the direction of the vote. Use 1 for an upvote and 0 for a downvote.",  # noqa: E501
         example=1,
