f20e8c5b322c5f83d592f9f5e9057839e90493ca
Ezequiel Parziale <eparziale@gmail.com>
2023-06-01 18:39:22 -0300

    :card_file_box: update: add timestamp mixin

---
+++ app/models/mixin.py
@@ -0,0 +1,15 @@
+from datetime import datetime
+
+from sqlalchemy.orm import Mapped, mapped_column
+from sqlalchemy.sql.expression import text
+from sqlalchemy.sql.sqltypes import TIMESTAMP
+
+
+class TimestampMixin:
+    created_at: Mapped[datetime] = mapped_column(
+        TIMESTAMP(timezone=True), server_default=text("now()"), nullable=False
+    )
+    updated_at: Mapped[datetime] = mapped_column(
+        TIMESTAMP(timezone=True), server_default=text("now()"), nullable=False,
+        onupdate=datetime.now()
+    )
