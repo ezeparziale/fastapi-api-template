0bc550ba4008d894e61d8ed4c77e22716d93abb9
Ezequiel Parziale <eparziale@gmail.com>
2024-05-03 20:51:38 -0300

    :rocket: update: replace passlib with bcrypt

--- app/utils/security.py
+++ app/utils/security.py
@@ -1,11 +1,35 @@
-from passlib.context import CryptContext
-
-pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+import bcrypt
 
 
 def get_password_hash(password: str) -> str:
-    return pwd_context.hash(password)
+    """
+    ### Generate a password hash using bcrypt.
+
+    Args:
+        password: The password to hash.
+
+    Returns:
+        str: The hashed password.
+    """
+    pwd_bytes = password.encode("utf-8")
+    salt = bcrypt.gensalt()
+    hashed_password = bcrypt.hashpw(password=pwd_bytes, salt=salt)
+    return hashed_password.decode("utf-8")
 
 
 def verify_password(plain_password: str, hashed_password: str) -> bool:
-    return pwd_context.verify(plain_password, hashed_password)
+    """
+    ### Verify if a plain password matches its hash.
+
+    Args:
+        plain_password: The plain password.
+        hashed_password: The hashed password.
+
+    Returns:
+        bool: True if the password matches, False otherwise.
+    """
+    hashed_password_bytes = hashed_password.encode("utf-8")
+    plain_password_bytes = plain_password.encode("utf-8")
+    return bcrypt.checkpw(
+        password=plain_password_bytes, hashed_password=hashed_password_bytes
+    )
