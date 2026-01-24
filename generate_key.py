2eef7f7a01940d2d0abebb20e3f2979ff970f43b
Ezequiel Parziale <eparziale@gmail.com>
2026-01-24 18:15:09 -0300

    :wrench: chore: replace print statement with logging for key generation process

--- generate_key.py
+++ generate_key.py
@@ -1,4 +1,12 @@
+import logging
+
 from cryptography.fernet import Fernet
 
+# Configure logging
+logging.basicConfig(level=logging.INFO)
+
+
+logging.info("Generating encryption key...")
 encryption_key = Fernet.generate_key().decode()
-print(f"ENCRYPTION_KEY={encryption_key}")
+logging.info(f"ENCRYPTION_KEY={encryption_key}")
+logging.info("Encryption key generated.")
